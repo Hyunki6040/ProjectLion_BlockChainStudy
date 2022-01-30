c1709f9efbbf2f5da47058fb837b33dda22c32cc
김현기 <hkgo6040@gmail.com>
2022-01-30 05:53:32 +0000

    first init

--- klay-market/src/App.js
+++ klay-market/src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 1 Smart contract 배포 주소 파악(가져오기)
+// 2 caver.js 이용해서 스마트 컨드랙트 연동하기
+// 3 가져온 스마트 컨트랙스 실행 결과(데이터) 웹에 표현하기
+
 function App() {
   return (
     <div className="App">
